1391d4c7a23fe06403c0ffd62f89ef640a027cd7
Kyle Gaither <30640336+KyleGaither@users.noreply.github.com>
2023-03-14 20:45:50 -0400

    - write order data to mongodb - add data persistance using docker named volume

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Order = require('./models/order');
 const app = express();
 const port = 3000;
 
-//const Order = require('./models/order');
-mongoose.connect('mongodb://mongo:27017/test');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://mongo:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo Connection Open");
+    })
+    .catch(err => {
+        console.log("CONNECTION ERROR:");
+        console.log(err);
+
+    });
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/order', function (req, res) {
-  console.log(req.body)
-  res.json(req.body)
+  var order = new Order(req.body);
+  order.save()
+    .then(order => {
+      console.log(order)
+    })
+    .catch(e => {
+        console.log(e)
+    })
+  res.redirect('/');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
