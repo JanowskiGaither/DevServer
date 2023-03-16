60a741f636c1a125434eaa0a8580fb393d9e9746
Kyle Gaither <30640336+KyleGaither@users.noreply.github.com>
2023-03-15 22:14:07 -0400

    - configure vite multi-page build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,12 @@ const {resolve} = require('path')
 const { defineConfig } = require('vite')
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        order: resolve(__dirname, '/orderForm/index.html'),
-        design: resolve(__dirname, '/designForm/index.html')
+        main: resolve(__dirname, 'src/index.html'),
+        order: resolve(__dirname, 'src/orderForm/index.html'),
+        design: resolve(__dirname, 'src/designForm/index.html')
       },
     },
   },
